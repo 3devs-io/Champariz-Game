7ec502cb58f1d2241081ef519008c67d1721375c
ReversedHourglass <tom.alegre@outlook.fr>
2020-02-02 12:22:52 +0100

    Usage of BlocBuilder to switch between views

--- lib/home.dart
+++ lib/home.dart
@@ -21,88 +21,88 @@ class _HomeState extends State<Home> {
         onWillPop: () => Future.value(false),
         child: Scaffold(
             backgroundColor: Theme.of(context).primaryColor,
-            body: SafeArea(
-                child: BlocListener<PlayerBloc, PlayerState>(
-              listener: (context, PlayerState state) {
-                if (state is InputNamesPlayer) {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      // return object of type Dialog
-                      return AlertDialog(
-                        title: Text(
-                          "Entrez le nom de chacun des participants à la partie",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        content: Text("Player names (" +
-                            state.playerNumber.toString() +
-                            ")", style: TextStyle(color: Colors.black)),
-                        actions: <Widget>[
-                          FlatButton(
-                            child: Text("Close"),
-                            onPressed: () {
-                              Navigator.pushNamed(context, HomeRoute);
-                            },
+            body: SafeArea(child:
+                BlocBuilder<PlayerBloc, PlayerState>(builder: (context, state) {
+              if (state is InputNamesPlayer) {
+                return Center(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: mediaQuery.size.height / 10,
+                      ),
+                      FlutterLogo(
+                        size: 250,
+                      ),
+                      SizedBox(
+                        height: mediaQuery.size.height / 30,
+                      ),
+                      Text(
+                        "Entrez le nom des différents joueurs",
+                        style: TextStyle(fontSize: 22.5),
+                      ),
+                      SizedBox(
+                        height: mediaQuery.size.height / 30,
+                      ),
+                    ],
+                  ),
+                );
+              }
+
+              if (state is SelectingNumberPlayers) {
+                return Center(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: mediaQuery.size.height / 10,
+                      ),
+                      FlutterLogo(
+                        size: 250,
+                      ),
+                      SizedBox(
+                        height: mediaQuery.size.height / 30,
+                      ),
+                      Text(
+                        "Jouer à Champariz",
+                        style: TextStyle(fontSize: 22.5),
+                      ),
+                      SizedBox(
+                        height: mediaQuery.size.height / 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.people),
+                          SizedBox(
+                            width: mediaQuery.size.width / 30,
+                          ),
+                          Text(
+                            "Nombre de joueurs",
+                            style: TextStyle(fontSize: 12.5),
                           ),
                         ],
-                      );
-                    },
-                  );
-                }
-              },
-              child: Center(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: mediaQuery.size.height / 10,
-                    ),
-                    FlutterLogo(
-                      size: 250,
-                    ),
-                    SizedBox(
-                      height: mediaQuery.size.height / 30,
-                    ),
-                    Text(
-                      "Jouer à Champariz",
-                      style: TextStyle(fontSize: 22.5),
-                    ),
-                    SizedBox(
-                      height: mediaQuery.size.height / 30,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(Icons.people),
-                        SizedBox(
-                          width: mediaQuery.size.width / 30,
-                        ),
-                        Text(
-                          "Nombre de joueurs",
-                          style: TextStyle(fontSize: 12.5),
-                        ),
-                      ],
-                    ),
-                    NumberPicker.integer(
-                        initialValue: _currentValue,
-                        minValue: 3,
-                        maxValue: 20,
-                        onChanged: (newValue) =>
-                            setState(() => _currentValue = newValue)),
-                    RaisedButton(
-                      onPressed: () {
-                        BlocProvider.of<PlayerBloc>(context)
-                            .add(SelectedNumber(_currentValue));
-                      },
-                      splashColor: Colors.white,
-                      elevation: 8.0,
-                      child: Text("Jouer"),
-                      color: Theme.of(context).accentColor,
-                      shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(30.0)),
-                    ),
-                  ],
-                ),
-              ),
-            ))));
+                      ),
+                      NumberPicker.integer(
+                          initialValue: _currentValue,
+                          minValue: 3,
+                          maxValue: 20,
+                          onChanged: (newValue) =>
+                              setState(() => _currentValue = newValue)),
+                      RaisedButton(
+                        onPressed: () {
+                          BlocProvider.of<PlayerBloc>(context)
+                              .add(SelectedNumber(_currentValue));
+                        },
+                        splashColor: Colors.white,
+                        elevation: 8.0,
+                        child: Text("Jouer"),
+                        color: Theme.of(context).accentColor,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0)),
+                      ),
+                    ],
+                  ),
+                );
+              }
+            }))));
   }
 }
