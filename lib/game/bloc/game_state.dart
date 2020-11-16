ad612f8f3eb046b1c44561d059a2d2d4aaafbbe0
ReversedHourglass <tom.alegre@outlook.fr>
2020-11-16 19:01:00 +0100

    Revert "Logic has been revamped"
    This reverts commit 247d35fe466a6548d64af37dbafc3d8a5d98832a.

--- lib/game/bloc/game_state.dart
+++ lib/game/bloc/game_state.dart
@@ -35,48 +35,13 @@ class GameError extends GameState {
   List<Object> get props => [];
 }
 
-class GiveDrinkState extends GameState {
-  final List<Player> players;
-  final Player player;
-  final int sips;
-
-  const GiveDrinkState(this.players, this.player, this.sips);
-
-  @override
-  List<Object> get props => [players, player, sips];
-}
-
-class FinishDrinkState extends GameState {
-  final Player player;
-
-  const FinishDrinkState(this.player);
-
-  @override
-  List<Object> get props => [player];
-}
-
-class EveryoneDrinkState extends GameState {
-  const EveryoneDrinkState();
-
-  @override
-  List<Object> get props => [];
-}
-
-class SoloDrinkState extends GameState {
-  final Player player;
-  final int sips;
-  const SoloDrinkState(this.player, this.sips);
-
-  @override
-  List<Object> get props => [player, sips];
-}
-
 class DrinkingGame extends GameState {
-  final Player player;
+  final String toDrink;
+  final List<Player> players;
   final bool isFinished;
 
-  const DrinkingGame(this.player, this.isFinished);
+  const DrinkingGame(this.toDrink, this.players, this.isFinished);
 
   @override
-  List<Object> get props => [player, isFinished];
+  List<Object> get props => [toDrink, players, isFinished];
 }
